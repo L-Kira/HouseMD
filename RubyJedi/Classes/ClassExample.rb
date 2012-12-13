4d93c62837d491be67be59ff29e6d53f9f9da27c
Desarrollo2 <Developer@TorchNetwork>
2012-12-13 10:57:14 -0600

    Agregado el constructor de clase con la lectura de atributos para la clase Jedi

--- RubyJedi/Classes/ClassExample.rb
+++ RubyJedi/Classes/ClassExample.rb
@@ -1,5 +1,11 @@
 class Jedi
+  attr_reader(:name, :age, :gender)
   def initialize(name, age, gender)
-    
+    @name = name
+    @age = age
+    @gender = gender
   end
 end
+
+jedi = Jedi.new("Obiwan",28,"male")
+puts jedi.name
